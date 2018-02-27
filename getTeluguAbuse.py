5bffddf1794809158cb168b368ff0992dfe4d68a
adarbha <adarbha@mtu.edu>
2018-02-27 13:24:14 -0500

    Added one more intro intent to the code
    Added an extra intent as intro intent to check how the code is handled

--- getTeluguAbuse.py
+++ getTeluguAbuse.py
@@ -1,21 +1,46 @@
+# Source code for responses - Teach Telugu App
 import random
-
-boothulu = ["Bahen-khe lowdey", "Bhaadakow","Erri pooka","Lunjhaa kodaka", "Muunda kodaka","Maa-khe lowdey"]
-
-
-
 def lambda_handler(event, context):
-    boothu = random.choice(boothulu)
     
-    response = {
+    # Event in args is basically the request json object from request generated
+    
+    abuseIntent = 'AbuseIntent'
+    introIntent = 'introduceInTelugu'
+
+    boothulu = ["Bahen-khe lowdey", "Bhaadakow","Erri pooka","Lunjhaa kodaka", "Muunda kodaka","Maa-khe lowdey"]
+
+            
+    def getAbuseIntent(boothulu):
+        boothu = random.choice(boothulu)
+        things_to_say = "Here you go, " + boothu
+        return buildresponse(things_to_say)
+        
+    
+    def getIntroIntent():
+        things_to_say = '''Naa peru Alexa, Maa mummy peru Sándhyaa Raani. Maa mummy ki iddharu daddys, So I don't know who my daddy is'''
+        return buildresponse(things_to_say)
+    
+        
+    def buildresponse(things_to_say):
+        response = {
         'version':'1.0',
         'response':{
             'outputSpeech':{
                 'type':'PlainText',
-                'text': "Here you go, " + boothu
-                
+                'text': things_to_say
+                }
             }
         }
-    }
-    
-    return response
+        return response
+        
+        
+    if(event['request']['type'] == 'IntentRequest'):
+        
+        if event['request']['intent']['name'] == abuseIntent:
+            # code abuse intent
+            return getAbuseIntent(boothulu)
+            
+        if event['request']['intent']['name'] == introIntent:
+            # code introIntent
+            return getIntroIntent()
+        
